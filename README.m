7176a0045f6dea3e0d865ef35f06deaadf2e3989
Richard Muia <145849801+Muhia3@users.noreply.github.com>
2023-12-20 13:26:06 -0800

    Create description.m
    work description

---
+++ README.m
@@ -0,0 +1,5 @@
+Generation of random numbers and application of simulation using r programming
+Plot a histogram of the means of the samples generated in part (a), using an appropriate option in R for plotting the histogram on the probability density scale.
+Plot the Probability Density Function (PDF) of the sampling distribution of the sample 
+Mean ,under the Central Limit Theorem (CLT), corresponding to the samples
+generated in part (a). The graph of the PDF should be superimposed on the histogram produced in part
